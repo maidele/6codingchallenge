9730404454a546e1e5b2c165661298c234e32649
Maidele Archelus <maidele@usf.edu>
2025-02-07 00:45:48 -0500

    I included dashes and comments in lines 1, 8, and 9

--- cc_6s.js
+++ cc_6s.js
@@ -1,7 +1,9 @@
+//Task 1 - Business Profit Calculation
+
 function calculateProfit(sellingPrice, costPrice, unitsSold) {
     return  sellingPrice - costPrice/unitsSold ;
 }
 calculateProfit(20, 30, 100); // Expected output: "Total Profit: $1000"
 calculateProfit(50, 70, 200); // Expected output: "Total Profit: $4000"
-console.log(`Profit: $${1000}`);  
-console.log(`Profit: $${4000}`)
+console.log(`Profit: $${1000}`);  // Results in a profit of $1000
+console.log(`Profit: $${4000}`); // Results in a profit of $400
