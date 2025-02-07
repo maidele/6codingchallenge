56ee1dad0ce722efdbfb06c2ba870a7f8087a49a
Maidele Archelus <maidele@usf.edu>
2025-02-07 01:46:19 -0500

    Solved the problem relating to business

--- cc_6s.js
+++ cc_6s.js
@@ -1,9 +1,16 @@
 //Task 1 - Business Profit Calculation
 
-function calculateProfit(sellingPrice, costPrice, unitsSold) {
-    return  sellingPrice - costPrice/unitsSold ;
+function calculateNetProfit(sellingPrice, costPrice, unitsSold) {
+    return sellingPrice - costPrice/unitsSold ;
 }
-calculateProfit(20, 30, 100); // Expected output: "Total Profit: $1000"
-calculateProfit(50, 70, 200); // Expected output: "Total Profit: $4000"
+const sellingPrice = 20; 50
+const costPrice = 30; 70
+const unitsSold = 40; 200
+calculateNetProfit(20, 30, 100);
+calculateNetProfit(50, 70, 200);
 console.log(`Profit: $${1000}`);  // Results in a profit of $1000
-console.log(`Profit: $${4000}`); // Results in a profit of $400
+console.log(`Profit: $${4000}`);  // Results in a profit of $4000
+
+
+
+
