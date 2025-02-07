0972ab6c2e1538059fec46789682f0f8db6a4eed
Maidele Archelus <maidele@usf.edu>
2025-02-07 02:13:47 -0500

    Task 2 - Sales Tax Computation

--- cc_6s.js
+++ cc_6s.js
@@ -1,7 +1,7 @@
 //Task 1 - Business Profit Calculation
 
 function calculateNetProfit(sellingPrice, costPrice, unitsSold) {
-    return sellingPrice - costPrice/unitsSold ;
+    return sellingPrice - costPrice*unitsSold ;
 }
 const sellingPrice = 20; 50
 const costPrice = 30; 70
@@ -11,6 +11,16 @@ calculateNetProfit(50, 70, 200);
 console.log(`Profit: $${1000}`);  // Results in a profit of $1000
 console.log(`Profit: $${4000}`);  // Results in a profit of $4000
 
+// Task 2- Sales Tax Computation
 
+function calculateSalesTax(Amount, taxRate) {
+    return Amount * taxRate;
+}
+totalAmount = 100; 500
+const taxRate = 0.07; 0.1 
+const tax = calculateSalesTax(100, 0.07);
+calculateSalesTax(500, 0.1)
+console.log(`SalesTax: $${7}`);  
+console.log(`SalesTax: $${50}`);
 
 
